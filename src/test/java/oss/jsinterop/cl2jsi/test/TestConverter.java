1314675a27d99721bd433b0c0af1c2bf70181ab8
didier <durand.didier@gmail.com>
2020-03-22 10:13:32 +0100

    convert async-2.0.js to jsinterop

--- src/test/java/oss/jsinterop/cl2jsi/test/TestConverter.java
+++ src/test/java/oss/jsinterop/cl2jsi/test/TestConverter.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.File;
 import java.io.IOException;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.google.googlejavaformat.java.FormatterException;
@@ -12,6 +13,9 @@ import com.google.googlejavaformat.java.FormatterException;
 import oss.jsinterop.cl2jsi.Converter;
 
 public class TestConverter {
+	
+	public static final String EXTERNS = "google-closure-compiler" + File.separator + "contrib" + File.separator + "externs";
+	public static final String MAPS = EXTERNS + File.separator + "maps";
 	
 	@Test
 	public void testCheckJar() throws Exception {
@@ -21,8 +25,16 @@ public class TestConverter {
 	
 	@Test
 	public void testConvertAsync_2_0() throws IOException, InterruptedException, FormatterException {
-		String jsFile = "async-2.0.js";
+		String jsFile = EXTERNS + File.separator + "async-2.0.js";
 		String className = "Async_2_0";
+		convert(jsFile,className);
+	}
+	
+	@Disabled("type Object missing")
+	@Test
+	public void testConvertGeoJson() throws IOException, InterruptedException, FormatterException {
+		String jsFile = EXTERNS + File.separator + "geojson.js";
+		String className = "GeoJson";
 		convert(jsFile,className);
 	}
 	
@@ -30,7 +42,21 @@ public class TestConverter {
 		Converter converter = new Converter();
 		String jsFilePath = Converter.JS_CODE_DIR + File.separator + jsFile;
 		String dependenciesFilePath = Converter.DEP_DIR + File.separator + jsFile + ".dep";
+		File depFile = new File(dependenciesFilePath);
+		if (!depFile.getParentFile().exists()) {
+			boolean created = depFile.getParentFile().mkdirs();
+			if (!created) {
+				throw new IOException("dep file dir not created: " + depFile.getParentFile().getAbsolutePath());
+			}
+		}
 		String generatedSourceJarPath = Converter.JARS_DIR + File.separator + jsFile +".jar";
+		File jarFile = new File(generatedSourceJarPath);
+		if (!jarFile.getParentFile().exists()) {
+			boolean created = jarFile.getParentFile().mkdirs();
+			if (!created) {
+				throw new IOException("jar dir not created: " + jarFile.getParentFile().getAbsolutePath());
+			}
+		}
 		String packageName = "oss.jsinterop.java";
 		int returnCode = converter.convert(jsFilePath,dependenciesFilePath, generatedSourceJarPath, packageName, className);
 		System.out.println("stdout:\n" + converter.getStdOut());
