2f5294d3b6d2e91715279877d8c194da91e32619
didier <durand.didier@gmail.com>
2020-03-22 08:34:23 +0100

    initial code load

--- src/main/java/oss/jsinterop/cl2jsi/Converter.java
+++ src/main/java/oss/jsinterop/cl2jsi/Converter.java
@@ -1,25 +1,34 @@
 package oss.jsinterop.cl2jsi;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Enumeration;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
 public class Converter {
 	
 	public static final String JS_CODE_DIR = System.getProperty("user.dir") + File.separator + "js";
 	public static final String GEN_CODE_DIR = System.getProperty("user.dir") + File.separator + "gen";
+	public static final String JARS_DIR = System.getProperty("user.dir") + File.separator + "jars";
+	public static final String DEP_DIR = System.getProperty("user.dir") + File.separator + "dep";
 	
 	public static final String GENERATOR_JAR = "ClosureJsinteropGenerator_deploy.jar";
 	public static final String GENERATOR_DIR = "jsinterop-generator-master-2020-03-21";
 	public static final String JAR_SUBPATH = "bazel-bin/java/jsinterop/generator/closure";
 	
 	private File generatorJar = null;
+	private String stdOut = null;
+	private String stdErr = null;
 	
 	public Converter() {
 		Path workingDir = Paths.get(System.getProperty("user.dir"));
@@ -42,47 +51,60 @@ public class Converter {
 		}
 	}
 	
-	public void convert(String jsFilePath, String dependenciesFilePath, String generatedSourceJarPath, String packageName, String className) {
-		String command = generatorJar.getAbsolutePath()  + "\n"
+	public int convert(String jsFilePath, String dependenciesFilePath, String generatedSourceJarPath, String packageName, String className) throws IOException, InterruptedException {
+		String command = "java -jar " + generatorJar.getAbsolutePath()  + "\n"
 				+ "  --debug_mode" + "\n"
 				+ "  --strict"  + "\n"
 				+ "  --output " + generatedSourceJarPath  + "\n"
 				+ "  --package_prefix " + packageName  + "\n"
+				+ "  --extension_type_prefix " + "foo"  + "\n" //TODO: understand what it is used for
 				+ "  --global_scope_class_name "	+ className  + "\n"
 				+ "  --output_dependency_file " + dependenciesFilePath  + "\n"
 			    + "  " + jsFilePath;
 		System.out.println("command: " + command);
 		command = command.replaceAll("\\n +", " ");
-		System.out.println("command: " + command);
+		//System.out.println("command: " + command);
+		Runtime runtime = Runtime.getRuntime();
+		Process process =  runtime.exec(command);
+		System.out.println("waiting for command execution...");
+	    process.waitFor();
+		int returnCode = process.exitValue();
+		InputStream stdoutStream = process.getInputStream();
+		stdOut = new BufferedReader(new InputStreamReader(stdoutStream)).lines().collect(Collectors.joining("\n"));
+		InputStream stderrStream = process.getErrorStream();
+		stdErr = new BufferedReader(new InputStreamReader(stderrStream)).lines().collect(Collectors.joining("\n"));
+		return returnCode;
 	}
 	
-	//source: https://www.programcreek.com/2012/08/unzip-a-jar-file-in-java-program/
-	public static void unzipJar(String destinationDir, String jarPath) throws IOException {
-		File file = new File(jarPath);
-		@SuppressWarnings("resource")
-		JarFile jar = new JarFile(file);
-		// fist get all directories,
-		// then make those directory on the destination Path
-		for (Enumeration<JarEntry> enums = jar.entries(); enums.hasMoreElements();) {
-			JarEntry entry = (JarEntry) enums.nextElement();
- 
-			String fileName = destinationDir + File.separator + entry.getName();
-			File f = new File(fileName);
- 
-			if (fileName.endsWith("/")) {
-				f.mkdirs();
-			}
+	public void unzipJar(String jarPath, String destinationDirPath) throws IOException {
+		File jarFile = new File(jarPath);
+		File destDir = new File(destinationDirPath);
+		System.out.println("jar to unzip: " + jarFile.getAbsolutePath());
+		System.out.println("dest dir for jar unzip: " + destDir.getAbsolutePath());
+		//check and setup desintation dir
+		if (!destDir.exists()) {
+			destDir.mkdirs();
+		} 
+		if (!destDir.isDirectory()) {
+			throw new IOException("not a directory: " + destDir.getAbsolutePath());
 		}
-		//now create all files
-		for (Enumeration<JarEntry> enums = jar.entries(); enums.hasMoreElements();) {
-			JarEntry entry = (JarEntry) enums.nextElement();
-			String fileName = destinationDir + File.separator + entry.getName();
-			File f = new File(fileName);
-			if (!fileName.endsWith("/")) {
-				InputStream is = jar.getInputStream(entry);
-				FileOutputStream fos = new FileOutputStream(f);
- 
-				// write contents of 'is' to 'fos'
+		//
+		@SuppressWarnings("resource")
+		JarFile jar = new JarFile(jarFile);
+		ArrayList<JarEntry> jarEntries = Collections.list(jar.entries());
+		for (JarEntry jarEntry : jarEntries) {
+			String fileName = destinationDirPath + File.separator + jarEntry.getName();
+			if (!fileName.contains("META-INF")) { //avoid jar specific files
+				System.out.println("file name: " + fileName);
+				File file = new File(fileName);
+				if (!file.getParentFile().exists()) {
+					boolean created = file.getParentFile().mkdirs();
+					if (!created) {
+						throw new IOException("parent dir not created: " + file.getParentFile().getAbsolutePath());
+					}
+				}
+				InputStream is = jar.getInputStream(jarEntry);
+				FileOutputStream fos = new FileOutputStream(file);
 				while (is.available() > 0) {
 					fos.write(is.read());
 				}
@@ -91,4 +113,12 @@ public class Converter {
 			}
 		}
 	}
+
+	public String getStdOut() {
+		return stdOut;
+	}
+
+	public String getStdErr() {
+		return stdErr;
+	}	
 }
--- src/test/java/oss/jsinterop/cl2jsi/test/TestConverter.java
+++ src/test/java/oss/jsinterop/cl2jsi/test/TestConverter.java
@@ -1,5 +1,9 @@
 package oss.jsinterop.cl2jsi.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+
 import org.junit.jupiter.api.Test;
 
 import oss.jsinterop.cl2jsi.Converter;
@@ -16,12 +20,16 @@ public class TestConverter {
 	public void testConvert() throws Exception  {
 		Converter converter = new Converter();
 		String jsFile = "async-2.0.js";
-		String jsFilePath = Converter.JS_CODE_DIR + jsFile;
-		String dependenciesFilePath = "foo.dep";
-		String generatedSourceJarPath = Converter.GEN_CODE_DIR + jsFile +".jar";
+		String jsFilePath = Converter.JS_CODE_DIR + File.separator + jsFile;
+		String dependenciesFilePath = Converter.DEP_DIR + File.separator + jsFile + ".dep";
+		String generatedSourceJarPath = Converter.JARS_DIR + File.separator + jsFile +".jar";
 		String packageName = "oss.jsinterop.java";
-		String className = "HeadClass";
-		converter.convert(jsFilePath,dependenciesFilePath, generatedSourceJarPath, packageName, className);
+		String className = "Async_2_0";
+		int returnCode = converter.convert(jsFilePath,dependenciesFilePath, generatedSourceJarPath, packageName, className);
+		System.out.println("stdout:\n" + converter.getStdOut());
+		System.out.println("stderr:\n" + converter.getStdErr());
+		assertEquals(0,returnCode);
+		converter.unzipJar(generatedSourceJarPath,Converter.GEN_CODE_DIR);
 	}
 
 }
